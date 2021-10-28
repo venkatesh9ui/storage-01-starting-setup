3d96ff3a115b8d9fde10ff134e3ac1060638d558
venkatesh <venkatesh9ui@gmail.com>
2021-10-27 21:24:17 -0400

    Getting Started with Cookies

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
-const userId = 'u123';
-const user = {
-  name: 'Max',
-  age: 30,
-  hobbies: ['sports', 'cooking'],
-};
-
 storeBtn.addEventListener('click', () => {
-  // localStorage.setItem('uid', userId);
-  sessionStorage.setItem('uid', userId);
-  localStorage.setItem('user', JSON.stringify(user));
+  const userId = 'u123';
+  document.cookie = `uid=${userId}`;
 });
 
 retrBtn.addEventListener('click', () => {
-  // const extractedId = localStorage.getItem('uid');
-  const extractedId = sessionStorage.getItem('uid');
-  const extractedUser = JSON.parse(localStorage.getItem('user'));
-  console.log(extractedUser);
-  if (extractedId) {
-    console.log('Got the id - ' + extractedId);
-  } else {
-    console.log('Could not find id.');
-  }
+  console.log(document.cookie);
 });
---
+++ localstorge.js
@@ -0,0 +1,27 @@
+const storeBtn = document.getElementById('store-btn');
+const retrBtn = document.getElementById('retrieve-btn');
+
+const userId = 'u123';
+const user = {
+  name: 'Max',
+  age: 30,
+  hobbies: ['sports', 'cooking'],
+};
+
+storeBtn.addEventListener('click', () => {
+  // localStorage.setItem('uid', userId);
+  sessionStorage.setItem('uid', userId);
+  localStorage.setItem('user', JSON.stringify(user));
+});
+
+retrBtn.addEventListener('click', () => {
+  // const extractedId = localStorage.getItem('uid');
+  const extractedId = sessionStorage.getItem('uid');
+  const extractedUser = JSON.parse(localStorage.getItem('user'));
+  console.log(extractedUser);
+  if (extractedId) {
+    console.log('Got the id - ' + extractedId);
+  } else {
+    console.log('Could not find id.');
+  }
+});
